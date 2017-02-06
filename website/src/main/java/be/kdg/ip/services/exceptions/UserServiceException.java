117b1616e2f308387f7d6354bbd6f10c683cd478
joricktriempont <jorick.triempont@student.kdg.be>
2017-02-06 09:45:29 +0100

    delete ex
    delete example site

--- website/src/main/java/be/kdg/ip/services/exceptions/UserServiceException.java
+++
@@ -1,9 +0,0 @@
-package be.kdg.ip.services.exceptions;
-
-/**
- * Created by wouter on 31.01.17.
- */
-public class UserServiceException extends Throwable {
-    public UserServiceException(String s) {
-    }
-}
